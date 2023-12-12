fc3330a84500fa1ae4f9ab4983e7decc221c4cf8
Onur ALACA <onuralcaa@gmail.com>
2023-12-12 13:36:05 +0300

    Arrays

---
+++ En_kucuk_rakami_bulma_diziler.asm
@@ -0,0 +1,47 @@
+org 100h
+
+    ;m1 ekrana yazdir
+    lea dx, m1
+    mov ah,9
+    int 21h
+
+    lea si , dizi[0] ; baslangic adresi alindi.
+    mov bl, '9'
+    
+    mov cx, 6
+    
+    d_yaz:    
+        mov dl,[si]
+        add dl, 30h
+        mov ah,2
+        int 21h
+        
+        cmp bl,dl ;sayilari karsilastirma
+        jl degis
+            mov bl,dl
+        degis:
+        
+        
+        
+        mov dl, ','
+        mov ah,2
+        int 21h
+        
+        inc si
+    loop d_yaz 
+
+        lea dx,m2
+        mov ah,9 
+        int 21h
+        
+        mov dl,bl
+        mov ah,2
+        int 21h
+        
+ret
+
+dizi db 5,2,8,6,0,1
+  
+m1 db 10,13, 'Dizi elemanlari : $'
+
+m2 db 10,13, 'En kucuk eleman : $'
